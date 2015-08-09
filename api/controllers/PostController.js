927d663d3c7dd34d1f106d35edf2b37d296c3d17
Matt Miller <mmiller@helixeducation.com>
2015-08-09 13:52:41 -0600

    Set up blueprints, models, and routes for blog posts

---
+++ api/controllers/PostController.js
@@ -0,0 +1,9 @@
+module.exports = {
+	mostRecent: function (req, res) {
+		Post.find().sort({ createdAt: -1 }).limit(1).exec(function (err, posts) {
+			if (err) return res.negotiate(err);
+
+			res.ok(posts[0] || null);
+		});
+	}
+};
---
+++ api/models/Post.js
@@ -0,0 +1,13 @@
+module.exports = {
+	attributes: {
+		title: {
+			type: 'string',
+			required: true
+		},
+		body: {
+			type: 'text',
+			required: true
+		}
+		// TODO: add slug, author, categories later on
+	}
+};
